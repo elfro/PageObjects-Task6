ba6afb51cb54128c34ab7cf0e4052b6bfbeafbdf
Ellina <dancalya@gmail.com>
2016-08-30 09:17:58 +0300

    reverted changes in pom.xml

--- src/test/java/tests/AllureTestListener.java
+++ src/test/java/tests/AllureTestListener.java
@@ -3,12 +3,22 @@ package tests;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 import ru.yandex.qatools.allure.annotations.Attachment;
 
 
 public class AllureTestListener extends TestListenerAdapter {
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+    }
 
     @Attachment(value = "{0}", type = "image/png")
     public byte[] makeScreenShot(String testName, WebDriver driver) {
@@ -20,5 +30,21 @@ public class AllureTestListener extends TestListenerAdapter {
         Object currentClass = result.getInstance();
         WebDriver driver = ((BaseTest) currentClass).driver;
         makeScreenShot(testName, driver);
+    }
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
     }
 }
