322389293650ace55efe49912c478ddab85bd1e7
Ellina <dancalya@gmail.com>
2016-09-11 11:52:05 +0300

    update all files

--- src/main/java/webDriver/PrepareDrivers.java
+++ src/main/java/webDriver/PrepareDrivers.java
@@ -6,7 +6,7 @@ public class PrepareDrivers {
     public static DesiredCapabilities capabilities;
 
     public static DesiredCapabilities prepareFirefox() {
-        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\geckodriver64.exe");
         capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", false);
         return capabilities;
@@ -18,3 +18,4 @@ public class PrepareDrivers {
         return capabilities;
     }
 }
+
--- src/test/java/tests/AllureTestListener.java
+++ src/test/java/tests/AllureTestListener.java
@@ -48,3 +48,4 @@ public class AllureTestListener extends TestListenerAdapter {
 
     }
 }
+
